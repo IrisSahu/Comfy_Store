94ba1e53b62074d6cce79dcf91b64f85686dde2f
Iris <prashusahu686@gmail.com>
2024-11-20 13:39:03 +0530

    first message

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,45 @@
+import express from "express";
+const router = express.Router();
+
+import {
+  createOrder,
+  getAllOrders,
+  getUserOrders,
+  countTotalOrders,
+  calculateTotalSales,
+  calcualteTotalSalesByDate,
+  findOrderById,
+  markOrderAsPaid,
+  markOrderAsDelivered,
+} from "../controllers/orderController.js";
+
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+
+// router
+//   .route("/")
+//   .post(authenticate, createOrder)
+//   .get(authenticate, authorizeAdmin, getAllOrders);
+
+  router
+  .route("/")
+  .post(createOrder)
+  .get(getAllOrders);
+
+router.route("/mine").get(getUserOrders);
+// router.route("/mine").get(authenticate, getUserOrders);
+router.route("/total-orders").get(countTotalOrders);
+router.route("/total-sales").get(calculateTotalSales);
+router.route("/total-sales-by-date").get(calcualteTotalSalesByDate);
+router.route("/:id").get(findOrderById);
+// router.route("/:id").get(authenticate, findOrderById);
+router.route("/:id/pay").put(markOrderAsPaid);
+// router.route("/:id/pay").put(authenticate, markOrderAsPaid);
+router
+  .route("/:id/deliver")
+  .put(markOrderAsDelivered);
+
+  // router
+  // .route("/:id/deliver")
+  // .put(authenticate, authorizeAdmin, markOrderAsDelivered);
+
+export default router;
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,53 @@
+import express from "express";
+import {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+  deleteUserById,
+  getUserById,
+  updateUserById,
+} from "../controllers/userController.js";
+
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+// router
+//   .route("/")
+//   .post(createUser)
+//   .get(authenticate, authorizeAdmin, getAllUsers);
+
+  router
+  .route("/")
+  .post(createUser)
+  .get(getAllUsers);
+
+router.post("/auth", loginUser);
+router.post("/logout", logoutCurrentUser);
+
+router
+  .route("/profile")
+  .get(getCurrentUserProfile)
+  .put(updateCurrentUserProfile);
+
+  // router
+  // .route("/profile")
+  // .get(authenticate, getCurrentUserProfile)
+  // .put(authenticate, updateCurrentUserProfile);
+
+// ADMIN ROUTES 👇
+router
+  .route("/:id")
+  .delete(deleteUserById)
+  .get(getUserById)
+  .put(updateUserById);
+
+  // router
+  // .route("/:id")
+  // .delete(authenticate, authorizeAdmin, deleteUserById)
+  // .get(authenticate, authorizeAdmin, getUserById)
+  // .put(authenticate, authorizeAdmin, updateUserById);
+export default router;
